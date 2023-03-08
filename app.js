40f7a93b438dd6cf10cbd406f96ad3599bed90e7
Jhonatan-S <studyprogrammer5@gmail.com>
2023-03-08 19:54:21 -0300

    Edit

---
+++ app.js
@@ -0,0 +1,69 @@
+// getting the player's name
+
+let namePlayer1 = window.prompt('Please! Enter your player name player 1 ')
+let namePlayer2 = window.prompt('Please! Enter your player name player 2 ')
+
+
+// Checking if the user has digited something 
+
+if (namePlayer1.length > 0 && namePlayer2.length > 0) {
+    // Writing the player's name in the html
+    document.querySelectorAll('.container  .dice  h2')[0].innerHTML = namePlayer1
+
+    document.querySelectorAll('.container  .dice  h2')[1].innerHTML = namePlayer2
+}
+
+
+
+function roll() {
+    document.querySelector('.container .result h2').style.visibility = 'hidden'
+    document.querySelectorAll('h3')[0].style.visibility = 'hidden'
+    document.querySelectorAll('h3')[1].style.visibility = 'hidden'
+
+    // play 1 image
+
+    let randomNumber1 = Math.floor(Math.random() * 6 + 1)
+
+    let randomDiceImage1 = "dice" + randomNumber1 + ".png" // dice2.png
+
+    let randomImageSource1 = "images/" + randomDiceImage1 // img/dice2.png
+
+    let image1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1)
+
+
+    // Play 2 image
+
+    let randomNumber2 = Math.floor(Math.random() * 6 + 1)
+
+    let randomDiceImage2 = "dice" + randomNumber2 + ".png" // dice2.png
+
+    let randomImageSource2 = "images/" + randomDiceImage2 // img/dice2.png
+
+    let image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+    // Condition winner
+
+    if (randomNumber1 > randomNumber2) {
+
+        document.querySelectorAll('h3')[0].style.color = 'green'
+        document.querySelectorAll('h3')[0].style.visibility = 'visible'
+        document.querySelectorAll('h3')[0].innerHTML = 'You win'
+        document.querySelectorAll('h3')[1].style.color = 'red'
+        document.querySelectorAll('h3')[1].innerHTML = 'You lose'
+        document.querySelectorAll('h3')[1].style.visibility = 'visible'
+
+    }
+    else if (randomNumber2 > randomNumber1) {
+
+        document.querySelectorAll('h3')[1].style.color = 'green'
+        document.querySelectorAll('h3')[1].style.visibility = 'visible'
+        document.querySelectorAll('h3')[1].innerHTML = 'You win'
+        document.querySelectorAll('h3')[0].style.color = 'red'
+        document.querySelectorAll('h3')[0].innerHTML = 'You lose'
+        document.querySelectorAll('h3')[0].style.visibility = 'visible'
+    }
+    else if (randomNumber2 == randomNumber1) {
+        document.querySelector('.container .result h2').style.visibility = 'visible'
+    }
+}
+
